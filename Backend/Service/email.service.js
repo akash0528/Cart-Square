ad365e276002a3f02458372fae0212921fbbbe53
akash0528 <akashdivakar852@.com>
2026-06-04 13:26:57 +0530

    update

--- Backend/Service/email.service.js
+++ Backend/Service/email.service.js
@@ -6,7 +6,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
